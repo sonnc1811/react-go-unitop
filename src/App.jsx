b7a18da38c2b18c7dd52ceb69d44c539ea46d0d4
sonnc1811 <canhson@gmail.com>
2026-04-04 20:33:17 +0700

    Welcome component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Welcome(){
   return (
+    <div>
+      <h1>Chao ban !</h1>
+  <p>Ban dang hoc React go tai Unitop.vn</p>
+    </div>
+  )
+}
+function App() {
+   return (
     <>
-      
-      <h1>Hello World - React Go Unitop.vn - Git + Github</h1>
-      <p>Phan Văn Cương</p>
-      
-    </>
+           <h1>React Go Unitop.vn</h1>
+           <Welcome />
+              </>
   )
 }
 
