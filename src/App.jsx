8d292209ca860874553125bfb641a7767798e1e3
sonnc1811 <canhson@gmail.com>
2026-04-06 20:30:30 +0700

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Welcome from "./components/Welcome";
+import { Header, Footer, Sidebar } from "./components/Layout";
 
-function Welcome(){
-  return (
-    <div>
-      <h1>Chao ban !</h1>
-  <p>Ban dang hoc React go tai Unitop.vn</p>
-    </div>
-  )
-}
 function App() {
-   return (
+  return (
     <>
-           <h1>React Go Unitop.vn</h1>
-           <Welcome />
-              </>
-  )
+      <Header />
+      <Welcome />
+      <Sidebar />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return <h1>Day la header</h1>;
+}
+function Footer() {
+  return <footer>Day la footer</footer>;
+}
+function Sidebar() {
+  return <div>Day la sidebar</div>;
+}
+export { Header, Footer, Sidebar };
