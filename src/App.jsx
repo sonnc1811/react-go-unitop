285fe71c69072f2bce2c50bbd7b2e106e8b5a72d
sonnc1811 <canhson@gmail.com>
2026-05-02 07:59:45 +0700

    Responsive tien loi

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-// import "./App.css";
-import Welcome from "./components/Welcome";
-import { Header, Footer, Sidebar } from "./components/Layout";
-
 import UserProfile from "./components/UserProfile";
 import { users } from "./data/users";
 import Button from "./components/Button";
+import "./App.css";
 
 function App() {
   //logic
@@ -25,15 +18,23 @@ function App() {
   //JSX =>UI
   return (
     <>
+      <div className="flex h-screen justify-center items-center">
+        <button className="bg-pink-500 text-white py-2 px-4 rounded-full hover:bg-pink-600 md:py-3 md:px-5 lg:px-8 cursor-pointer">
+          Click me
+        </button>
+      </div>
+      {/* <h1 className="text-2xl text-red-900 hover:text-blue-700">
+        Hello Tailwind - React Go
+      </h1> */}
       {/* <Button>
         👉Dang ky <strong>nhan uu dai ngay</strong>
       </Button>
       <Button>🔑Dang nhap</Button> */}
-      {FilteredUser?.length > 0 ? (
+      {/* {FilteredUser?.length > 0 ? (
         FilteredUser.map((user) => <UserProfile key={user.id} user={user} />)
       ) : (
         <p>Khong tim thay du lieu</p>
-      )}
+      )} */}
     </>
   );
 }
